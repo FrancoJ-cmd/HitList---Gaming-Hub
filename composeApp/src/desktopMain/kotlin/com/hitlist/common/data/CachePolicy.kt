0a253c2b43cc5fc17d7a370eb952f5ba66f307d8
franco <franco@francoj.dev>
2026-06-13 19:18:10 -0300

    reducir TTL del caché de GameDetail de 24 h a 2 min

--- composeApp/src/desktopMain/kotlin/com/hitlist/common/data/CachePolicy.kt
+++ composeApp/src/desktopMain/kotlin/com/hitlist/common/data/CachePolicy.kt
@@ -4,7 +4,7 @@ object CachePolicy {
     const val SEED_LIST_TTL_MS = 24 * 60 * 60 * 1000L
     const val LIVE_PLAYERS_TTL_MS = 60 * 1000L
     const val REVIEWS_TTL_MS = 6 * 60 * 60 * 1000L
-    const val METADATA_TTL_MS = 24 * 60 * 60 * 1000L
+    const val METADATA_TTL_MS = 2 * 60 * 1000L
     const val DEALS_TTL_MS = 60 * 60 * 1000L
     const val NEWS_TTL_MS = 30 * 60 * 1000L
 
